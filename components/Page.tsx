cc4271069c39c40ac0b700fbd8758ffbb0a5eb1f
Shaderpixel <lock.j.h@gmail.com>
2022-12-14 18:57:39 -0500

    refactor: main is set on the _app level, use article instead

--- components/Page.tsx
+++ components/Page.tsx
@@ -8,10 +8,10 @@ import { StoryBlokComponentProps } from '../types';
 
 export const Page = ({ blok }: StoryBlokComponentProps) => {
 	return (
-		<main {...storyblokEditable(blok)}>
+		<article {...storyblokEditable(blok)}>
 			{blok.body?.map((nestedBlok: SbBlokData) => (
 				<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
 			))}
-		</main>
+		</article>
 	);
 };
